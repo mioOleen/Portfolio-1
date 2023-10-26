f0116efc3c1727a505a5c55d2bed35f580bd25e1
zintheint <zintheint228@gmail.com>
2023-10-26 11:06:15 +0630

    hamburger menu for mobile nav

---
+++ app.js
@@ -0,0 +1,25 @@
+const navBar = document.querySelector('.navBar');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const pf = document.querySelector('#pf');
+const abt= document.querySelector('#abt');
+const cont = document.querySelector('#cont');
+
+
+function show() {
+  closeMenu.style.display="block";
+  openMenu.style.display="none";
+  navBar.style.display = 'flex';
+  navBar.style.top = '0';
+}
+function close() {
+  navBar.style.top = '-100%';
+  openMenu.style.display="block";
+  closeMenu.style.display="none";
+}
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+pf.addEventListener('click', close);
+abt.addEventListener('click', close);
+cont.addEventListener('click', close);
